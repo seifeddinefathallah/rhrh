4e2d8ffcc2951cec6be8883c2e5949d19cbfcf01
rayenkharbeche <rayen.kharbeche@esprit.tn>
2024-08-13 13:51:42 +0100

    leave request search

--- OneDrive - CSI/Bureau/APP RH/resources/views/dashboard.blade.php
+++ OneDrive - CSI/Bureau/APP RH/resources/views/dashboard.blade.php
@@ -10,18 +10,14 @@
           <div class="d-flex align-items-end row">
             <div class="col-sm-7">
               <div class="card-body">
-                <h5 class="card-title text-primary">Hello {{ Auth::user()->name }}! 🎉</h5>
-                <p class="mb-4">
-                  You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in your profile.
-                </p>
-                <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+                <h5 class="card-title text-primary">Hello {{ Auth::user()->name }}! </h5>
+            
               </div>
             </div>
           </div>
         </div>
       </div>
 
-     <!-- Leave Balances -->
 <!-- Leave Balances -->
 <div class="col-lg-4 col-md-4 order-1">
   <div class="row">
@@ -44,7 +40,7 @@
                 </div>
               </div>
             </div>
-            <h5 class="card-title mt-3">{{ $balance->name }}</h5>
+            <h5 class="card-title mt-3">Congé {{ $balance->name }}</h5>
             <p class="mb-1">Jours restants: <span class="fw-bold">{{ $balance->remaining_days }} J</span></p>
           </div>
         </div>
@@ -55,107 +51,7 @@
 
 
 
-      <!-- Total Revenue -->
-      <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
-        <div class="card">
-          <div class="row row-bordered g-0">
-            <div class="col-md-8">
-              <h5 class="card-header m-0 me-2 pb-3">Total Revenue</h5>
-              <div id="totalRevenueChart" class="px-2"></div>
-            </div>
-            <div class="col-md-4">
-              <div class="card-body">
-                <div class="text-center">
-                  <div class="dropdown">
-                    <button class="btn btn-sm btn-outline-primary dropdown-toggle" type="button" id="growthReportId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      2022
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="growthReportId">
-                      <a class="dropdown-item" href="javascript:void(0);">2021</a>
-                      <a class="dropdown-item" href="javascript:void(0);">2020</a>
-                      <a class="dropdown-item" href="javascript:void(0);">2019</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div id="growthChart"></div>
-              <div class="text-center fw-semibold pt-3 mb-2">62% Company Growth</div>
-              <div class="d-flex px-xxl-4 px-lg-2 p-4 gap-xxl-3 gap-lg-1 gap-3 justify-content-between">
-                <div class="d-flex">
-                  <div class="me-2">
-                    <span class="badge bg-label-primary p-2"><i class="bx bx-dollar text-primary"></i></span>
-                  </div>
-                  <div class="d-flex flex-column">
-                    <small>2022</small>
-                    <h6 class="mb-0">$32.5k</h6>
-                  </div>
-                </div>
-                <div class="d-flex">
-                  <div class="me-2">
-                    <span class="badge bg-label-info p-2"><i class="bx bx-wallet text-info"></i></span>
-                  </div>
-                  <div class="d-flex flex-column">
-                    <small>2021</small>
-                    <h6 class="mb-0">$41.2k</h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Other Metrics -->
-      <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
-        <div class="row">
-          <div class="col-6 mb-4">
-            <div class="card">
-              <div class="card-body">
-                <div class="card-title d-flex align-items-start justify-content-between">
-                  <div class="avatar flex-shrink-0">
-                    <img src="../backend/assets/img/icons/unicons/paypal.png" alt="Payments" class="rounded" />
-                  </div>
-                  <div class="dropdown">
-                    <button class="btn p-0" type="button" id="cardOpt4" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="bx bx-dots-vertical-rounded"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
-                </div>
-                <span class="d-block mb-1">Payments</span>
-                <h3 class="card-title text-nowrap mb-2">$2,456</h3>
-                <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
-              </div>
-            </div>
-          </div>
-          <div class="col-6 mb-4">
-            <div class="card">
-              <div class="card-body">
-                <div class="card-title d-flex align-items-start justify-content-between">
-                  <div class="avatar flex-shrink-0">
-                    <img src="../backend/assets/img/icons/unicons/cc-primary.png" alt="Revenue" class="rounded" />
-                  </div>
-                  <div class="dropdown">
-                    <button class="btn p-0" type="button" id="cardOpt1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="bx bx-dots-vertical-rounded"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt1">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
-                </div>
-                <span class="d-block mb-1">Revenue</span>
-                <h3 class="card-title text-nowrap mb-2">$7,890</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +11.28%</small>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+     
     </div>
   </div>
 </div>
